1f99ccb28b73e60b304485a03d60afec7fcb9160
mpicat <maxime.picat@coursdiderot.com>
2022-05-29 20:00:03 +0200

    MAJ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'CV de Maxime PICAT';
+  title = 'CV Maxime PICAT';
 
   toggleClass: boolean = false;
 }
